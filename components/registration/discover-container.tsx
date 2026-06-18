66679a7cf4d4d462e1f08cc97f61e18f64bf3b7a
Shaurya Bisht <shauryabisht707@gmail.com>
2026-06-18 21:05:36 +0530

    format

--- components/registration/discover-container.tsx
+++ components/registration/discover-container.tsx
@@ -856,6 +856,7 @@ export function DiscoverContainer() {
                                         </span>
                                       )}
                                     </div>
+                                    
                                     <div className="flex items-center gap-[12px]">
                                       <span className="font-['Google_Sans_Flex',sans-serif] text-[12px] text-white opacity-60">
                                         {team.currentMembers}/{team.maxMembers}{" "}
